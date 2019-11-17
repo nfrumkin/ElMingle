8cb3e5542cc77061c99a8be90159c341ab18cf51
Norman A. Toro Vega <normant@bu.edu>
2019-11-16 21:27:03 -0500

    Conference Call Working

--- answer_phone.py
+++ answer_phone.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
-from twilio.twiml.voice_response import VoiceResponse, Record
+from twilio.twiml.voice_response import VoiceResponse, Record, Dial
 
 app = Flask(__name__)
 
 recordingURL = ""
+MODERATOR = '+19392190769'
 
 @app.route("/answer", methods=['GET', 'POST'])
 def answer_call():
@@ -30,8 +31,22 @@ def recording_received():
     print(request.form["RecordingUrl"])
     recordingURL = request.form["RecordingUrl"]
     resp = VoiceResponse()
-    resp.say("Nice to meet you. In just a few moments you'll be speaking to the granpapi ro granmommy of your dreams!", voice='Polly.Emma')
+    resp.say("Nice to meet you. In just a few moments you'll be speaking to the granpapi or granmommy of your dreams!", voice='Polly.Emma')
 
+    # Start with a <Dial> verb
+    with Dial() as dial:
+        # If the caller is our MODERATOR, then start the conference when they
+        # join and end the conference when they leave
+        if request.values.get('From') == MODERATOR:
+            dial.conference(
+                'My conference',
+                start_conference_on_enter=True,
+                end_conference_on_exit=True)
+        else:
+            # Otherwise have the caller join as a regular participant
+            dial.conference('My conference', start_conference_on_enter=False)
+
+    resp.append(dial)
     return str(resp)
 
 if __name__ == "__main__":
